82ec52d5479419d32f9f6e14ff4d9f5dda339432
unknown <00035919@uca.edu.sv>
2021-06-26 01:44:24 -0600

    Finished FmrPrincipal

--- Gestion_Citas_Covid19/Properties/Resources.Designer.cs
+++ Gestion_Citas_Covid19/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Gestion_Citas_Covid19.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap flecha {
+            get {
+                object obj = ResourceManager.GetObject("flecha", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logo_covid_SD {
             get {
                 object obj = ResourceManager.GetObject("logo-covid SD", resourceCulture);
